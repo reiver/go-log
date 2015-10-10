5c25e91116ca32736c5e43b4bcf5bcee8ed32626
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-10-10 02:43:53 -0700

    added docs

--- filtered_router.go
+++ filtered_router.go
@@ -1,6 +1,12 @@
 package flog
 
 
+// NewFilteredRouter returns an initialized FilteredRouter.
+//
+// 'subrouter' is the sub-router that a FilteredRouter will
+// re-Route a 'message' (and 'context') to, but only on the
+// condition that 'filterFn' returns 'true' for the 'message'
+// and 'context' passed to it.
 func NewFilteredRouter(subrouter Router, filterFn func(string, map[string]interface{})bool) *FilteredRouter {
 	router := FilteredRouter{
 		subrouter:subrouter,
@@ -11,6 +17,7 @@ func NewFilteredRouter(subrouter Router, filterFn func(string, map[string]interf
 }
 
 
+// FilteredRouter is a Router that conditionally routes or discards a message (and its context).
 type FilteredRouter struct {
 	subrouter Router
 	filterFn func(string, map[string]interface{})bool
