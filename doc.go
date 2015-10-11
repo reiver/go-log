5b4df795d18ec8923d42b5dd0e1418b28826fffc
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-10-10 17:28:51 -0700

    added to docs

--- doc.go
+++ doc.go
@@ -9,7 +9,7 @@ Basic usage of the flogger looks like this:
 	
 	flogger := flog.New(router)
 
-Once you have this, you can do things such as:
+Once you have the flogger, you can do things such as:
 
 	flogger.Print("Hello world!")
 	flogger.Println("Hello world!")
@@ -22,6 +22,36 @@ Once you have this, you can do things such as:
 	flogger.Fatal("Something really bad happened!")
 	flogger.Fatalln("Something really bad happened!")
 	flogger.Fatalf("Something really bad happened: %s.", problemDescription)
+
+
+Deployment Environment
+
+Of course in a real application system you should (probably) create a different kind
+of flogger for each deployment environment.
+
+For example:
+
+	var flogger flog.Flogger
+	
+	switch deploymentEnvironment {
+	case "DEV":
+		router := NewPrettyWritingRouter(os.Stdout)
+		
+		flogger = flog.New(router)
+	case "PROD":
+		verboseRouter = flog.NewDiscardingRouter()
+		if isVerboseMode {
+			verboseRouter = NewCustomVerboseRouter()
+		}
+
+		panicDetectionRouter := flog.NewFilteringRouter(NewCustomerPanicRecordingRouter(), filterOnlyPanicsFunc)
+
+		errorDetectionRouter := flog.NewFilteringRouter(NewCustomerPanicRecordingRouter(), filterOnlyErrorsFunc)
+
+		router := NewFanoutRouter(verboseRouter, panicDetectionRouter, errorDetectionRouter)
+		
+		flogger = flog.New(router)
+	}
 
 
 */
