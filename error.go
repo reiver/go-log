2e14e0b46deaa19f5b275bd1d0b05b3dde93f5c9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-07-27 14:51:04 -0700

    madeit so that for flog.Error(), flog.Errorf(), flog.Errorln() that anything that fits an error that is passed at a parameter gets collected into the "~errors" key in the context. the thing under the "~errors" key is a []error.

--- error.go
+++ error.go
@@ -17,14 +17,18 @@ func (flogger *internalFlogger) Error(v ...interface{}) {
 
 	msg := fmt.Sprint(v...)
 
-	flogger.route(msg, errorContext)
+	errCtx := newErrorContext(errorContext, v...)
+
+	flogger.route(msg, errCtx)
 }
 
 func (flogger *internalFlogger) Errorf(format string, v ...interface{}) {
 
 	msg := fmt.Sprintf(format, v...)
 
-	flogger.route(msg, errorContext)
+	errCtx := newErrorContext(errorContext, v...)
+
+	flogger.route(msg, errCtx)
 }
 
 func (flogger *internalFlogger) Errorfe(err error, format string, v ...interface{}) {
@@ -44,5 +48,39 @@ func (flogger *internalFlogger) Errorln(v ...interface{}) {
 
 	msg := fmt.Sprintln(v...)
 
-	flogger.route(msg, errorContext)
+	errCtx := newErrorContext(errorContext, v...)
+
+	flogger.route(msg, errCtx)
+}
+
+
+func newErrorContext(baseContext map[string]interface{}, v ...interface{}) map[string]interface{} {
+
+
+	// Collect any errors.
+	errs := []error{}
+	for _, datum := range v {
+		if err, ok := datum.(error); ok {
+			errs = append(errs, err)
+		}
+	}
+
+
+	if 0 == len(errs) {
+		return baseContext
+	}
+
+
+	// Copy the base context.
+	context := map[string]interface{}{}
+	for k,v := range baseContext {
+		context[k] = v
+	}
+
+
+	// Put the collected errors in this new context.
+	context["~errors"] = errs
+
+
+	return context
 }
