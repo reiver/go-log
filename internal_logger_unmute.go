da98f624381ffee6df101fae82075f266a4018c7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-02-04 18:54:00 -0800

    logger

---
+++ internal_logger_unmute.go
@@ -0,0 +1,33 @@
+package flog
+
+func (receiver *internalLogger) UnmuteDebug() {
+	receiver.mutedDebug = false
+}
+
+func (receiver *internalLogger) UnmuteError() {
+	receiver.mutedError = false
+}
+
+func (receiver *internalLogger) UnmuteFatal() {
+	receiver.mutedFatal = false
+}
+
+func (receiver *internalLogger) UnmuteHighlight() {
+	receiver.mutedHighlight = false
+}
+
+func (receiver *internalLogger) UnmuteInform() {
+	receiver.mutedInform = false
+}
+
+func (receiver *internalLogger) UnmutePanic() {
+	receiver.mutedPanic = false
+}
+
+func (receiver *internalLogger) UnmuteTrace() {
+	receiver.mutedTrace = false
+}
+
+func (receiver *internalLogger) UnmuteWarn() {
+	receiver.mutedWarn = false
+}
--- logger.go
+++ logger.go
@@ -4,41 +4,49 @@ type Logger interface {
 	Debug(...interface{})
 	Debugf(string, ...interface{})
 	MuteDebug()
+	UnmuteDebug()
 	DebugMuted() bool
 
 	Error(...interface{}) error
 	Errorf(string, ...interface{}) error
 	MuteError()
+	UnmuteError()
 	ErrorMuted() bool
 
 	Fatal(...interface{})
 	Fatalf(string, ...interface{})
 	MuteFatal()
+	UnmuteFatal()
 	FatalMuted() bool
 
 	Highlight(...interface{})
 	Highlightf(string, ...interface{})
 	MuteHighlight()
+	UnmuteHighlight()
 	HighlightMuted() bool
 
 	Inform(...interface{})
 	Informf(string, ...interface{})
 	MuteInform()
+	UnmuteInform()
 	InformMuted() bool
 
 	Panic(...interface{})
 	Panicf(string, ...interface{})
 	MutePanic()
+	UnmutePanic()
 	PanicMuted() bool
 
 	Trace(...interface{})
 	Tracef(string, ...interface{})
 	MuteTrace()
+	UnmuteTrace()
 	TraceMuted() bool
 
 	Warn(...interface{})
 	Warnf(string, ...interface{})
 	MuteWarn()
+	UnmuteWarn()
 	WarnMuted() bool
 
 	Prefix(...string) Logger
